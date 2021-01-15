e606085f35db77936ab339127654e0ea5150b653
luuidy <luidy.informatica@gmail.com>
2021-01-15 15:47:17 -0300

    sessions

--- src/app.js
+++ src/app.js
@@ -2,20 +2,25 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const flash = require('req-flash')
-
-const porta = process.env.PORT || 3000
+let session = require('express-session')
+let port = process.env.PORT || 3000
 
 const app = express()
 
 // middlewares
+app.use(session({secret: '123', resave: true, saveUninitialized: true}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(flash())
+app.use(express.static(path.join("src", "public")))
 
 // settings
 app.set("view engine", "ejs")
 app.set("view", 'src/views')
 
-app.use(express.static(path.join("src", "public")))
+app.get('/', (req, res)=> {
+    res.send('hello world');
+});
 
-app.listen(porta, console.log('Server On'))
+
+app.listen(port, console.log(`Server On: http://localhost:${port}`))
